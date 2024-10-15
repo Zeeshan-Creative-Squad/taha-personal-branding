47586b694f492481bedec938be5f919da5be6242
Muhammad Zeeshan <zeeshan.creativesquad@gmail.com>
2024-10-15 05:03:48 +0500

    Work in Progress

--- frontend/src/components/OurTestimonials/ConnectPopup.js
+++ frontend/src/components/OurTestimonials/ConnectPopup.js
@@ -1,6 +1,8 @@
 import "./ConnectPopup.css"
+import { useNavigate } from "react-router-dom";
 
 function ConnectPopup() {
+  const navigate = useNavigate();
   return (
     <div className="main-container">
       <div className="container">
@@ -10,7 +12,7 @@ function ConnectPopup() {
               <h3 className="head-h1">Have projects in mind? Let’s work together</h3>
               <p className="para_main">Lorem Ipsum has been the printer took a galley of type and scrambled survived not only five finance software has love the financial haveing on centuries Lorem Ipsum has been the rinter took a galley of type and scrambled survived</p>
               <div className="button-section">
-                <button className="connect-button">Let's Connect</button>
+              <button onClick={() => navigate("/contact")} className="connect-button">Let's Connect</button>
               </div>
             </div>
           </div>
